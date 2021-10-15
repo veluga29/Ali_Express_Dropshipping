6751004c55de0f85f4f880fabdc2609fb74df948
veluminous <sungyoon606@naver.com>
2021-10-16 03:00:33 +0900

    Installed bootstrap

--- aed-ui/pages/_app.tsx
+++ aed-ui/pages/_app.tsx
@@ -1,7 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
+import Head from "next/head"
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
